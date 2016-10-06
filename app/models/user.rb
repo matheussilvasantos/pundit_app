da2026d1ace58ca17edc6b6d13f4568c7f0d75b4
Matheus Silva <oliveira.matheussilva@outlook.com>
2016-10-05 22:29:40 -0300

    setup user model for roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  enum role: [:directors, :sales, :user]
+  after_initialize :set_default_role, :if => :new_record?
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+
+  private
+
+    def set_default_role
+      self.role ||= :user
+    end
 end
